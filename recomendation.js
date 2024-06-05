72259271428bff08e675b28a756dd8c55ff11272
Owl of Chaos <114159412+OwlofChaos@users.noreply.github.com>
2024-06-05 23:16:23 +0700

    Update recomendation.js

--- recomendation.js
+++ recomendation.js
@@ -1,17 +1,8 @@
-function changeColor(button, category) {
+function changeColor(button) {
     var buttons = document.querySelectorAll('.btn');
     buttons.forEach(function(btn) {
         btn.classList.remove('active');
     });
 
     button.classList.add('active');
-
-    var messages = document.querySelectorAll('.message');
-    messages.forEach(function(message) {
-        if (category === 'all' || message.dataset.category === category) {
-            message.style.display = 'flex';
-        } else {
-            message.style.display = 'none';
-        }
-    });
 }
