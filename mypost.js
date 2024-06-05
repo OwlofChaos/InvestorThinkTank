34fb71cb2cedee38e633150a2c80eff5782fb224
Owl of Chaos <114159412+OwlofChaos@users.noreply.github.com>
2024-06-05 22:17:39 +0700

    Update mypost.js

--- mypost.js
+++ mypost.js
@@ -1,25 +1,27 @@
-function loadMessagesFromLocalStorage() {
-            const savedMessages = JSON.parse(localStorage.getItem('messages')) || [];
-            const messagesContainer = document.getElementById('messages');
+    function loadMessagesFromLocalStorage() {
+        const savedMessages = JSON.parse(localStorage.getItem('messages')) || [];
+        const messagesContainer = document.getElementById('messages');
 
-            savedMessages.forEach(msg => {
-                const messageElement = document.createElement('div');
-                messageElement.className = 'message';
-                let messageContent = `
-                    <div class="avatar"></div>
-                    <div class="text-content">
-                        <div class="username">${msg.username}</div>
-                `;
-                if (msg.text) {
-                    messageContent += `<div class="message-text">${msg.text}</div>`;
-                }
-                if (msg.image) {
-                    messageContent += `<div class="message-image"><img src="${msg.image}" alt="Posted Image"></div>`;
-                }
-                messageElement.innerHTML = messageContent + `</div>`;
-                messagesContainer.appendChild(messageElement);
-            });
+        savedMessages.forEach(msg => {
+            const messageElement = document.createElement('div');
+            messageElement.className = 'message';
+            let messageContent = `
+                <div class="avatar"></div>
+                <div class="text-content">
+                    <div class="username">${msg.username}</div>
+            `;
+            if (msg.text) {
+                messageContent += `<div class="message-text">${msg.text}</div>`;
+            }
+            if (msg.image) {
+                messageContent += `<div class="message-image"><img src="${msg.image}" alt="Posted Image"></div>`;
+            }
+            messageElement.innerHTML = messageContent + `</div>`;
+            messagesContainer.appendChild(messageElement);
+        });
 
-            // Scroll to the bottom of the messages container
-            messagesContainer.scrollTop = messagesContainer.scrollHeight;
-        }
+        // Scroll to the bottom of the messages container
+        messagesContainer.scrollTop = messagesContainer.scrollHeight;
+    }
+
+    document.addEventListener('DOMContentLoaded', loadMessagesFromLocalStorage);
